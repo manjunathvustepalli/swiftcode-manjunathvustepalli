46c10b56cbf31430ada254c85e1274b03fddc717
manjunathvustepalli <manjunathvustepalli@gmail.com>
2018-04-14 12:18:14 +0000

    front end

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,5 +1,14 @@
 var app = angular.module('chatApp', ['ngMaterial']);
 app.controller('chatController', function ($scope) {
+    $scope.messages = [{
+            'sender': 'USER',
+            'text': 'hello'
+},
+        {
+            'sender': 'BOT',
+            'text': 'hii what can i do for u?'
+}
 
+];
 
 });
